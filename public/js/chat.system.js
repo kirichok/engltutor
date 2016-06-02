f8b39b3f23d19eea8ff7d95831709e315c66b117
kirichok <evgeniy.kirichok@gmail.com>
2016-06-02 10:04:15 +0300

    chat ws/ws

--- public/js/chat.system.js
+++ public/js/chat.system.js
@@ -80,7 +80,7 @@ ChatClient.prototype.createConnection = function (reconnect) {
 
     console.log('Try to connect');
 
-    self.socket = new WebSocket('ws://'+window.location.hostname+':3001/');
+    self.socket = new WebSocket('ws://'+window.location.hostname+':3001/ws');
 
     self.socket.onopen = function () {
         console.log('connected');
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-//var websocket = require('websocket');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -70,70 +69,5 @@ wss.on('connection', function(socket){
         });
     }
 });
-
-/*
-var ws = new websocket.server({
-  httpServer: server,
-  autoAcceptConnections: false
-});
-
-ws.on('request', function (req) {
-  var connection = req.accept('', req.origin);
-  clients.push(connection);
-  console.log('Connected ' + connection.remoteAddress);
-
-  connection.on('message', function (message) {
-
-    var dataName = message.type + 'Data',
-        data = JSON.parse(message[dataName]);
-
-    switch (data.header) {
-      case 'message':
-        sendMessageAllClients(message[dataName], false);
-        break;
-      case 'updateUsers':
-        clients[clients.indexOf(connection)].chatUserName = data.user;
-        updateUsersList();
-        break;
-
-    }
-
-    console.log('Received: header-' + data.header + ', user-' + data.user + ', message-' + data.message);
-  });
-  connection.on('close', function (reasonCode, description) {
-    console.log('Disconnected ' + connection.remoteAddress);
-
-    var clientId = clients.indexOf(connection);
-    if (clientId != -1) {
-      clients.splice(clientId, 1);
-    }
-
-    updateUsersList();
-  });
-
-  function updateUsersList() {
-    var chatUsers = [];
-
-    clients.forEach(function (client) {
-      chatUsers.push(client.chatUserName);
-    });
-
-    data = {
-      header: 'updateUsers',
-      users: chatUsers
-    };
-
-    sendMessageAllClients(JSON.stringify(data), true);
-  }
-
-  function sendMessageAllClients(data, ignoreCurrent) {
-    clients.forEach(function (client) {
-      if (ignoreCurrent || connection !== client) {
-        client.send(data);
-      }
-    });
-  }
-});
-*/
 
 module.exports = router;
